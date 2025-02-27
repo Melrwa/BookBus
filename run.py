bc0a908857c409c1918ae439211b251b2d52bd62
melrwa <orwamelkizedek@gmail.com>
2025-02-27 16:28:33 +0300

    feat: update run configuration to use environment variable for port and enable debug mode

--- run.py
+++ run.py
@@ -1,9 +1,20 @@
+# from server.app import create_app
+
+# app = create_app()
+
+# if __name__ == "__main__":
+#     with app.app_context():
+#         app.run(debug=True)
+
+
 from server.app import create_app
+import os
 
 app = create_app()
 
 if __name__ == "__main__":
-    with app.app_context():
-        app.run(debug=True)
-
-
+    # Get the port from the environment variable (required for Render)
+    port = int(os.getenv("PORT", 5000))
+    
+    # Run the app
+    app.run(host="0.0.0.0", port=port, debug=app.config["DEBUG"])
