106a9305269de72d76a09f213b1d80860019349e
ShirleenChebet <shirlynshirley9@gmail.com>
2025-03-04 09:36:33 +0300

    Add CardContent import to AdminManageBuses component

--- client/src/app/adminhomepage/adminmanagebuses/Adminmanagebuses.js
+++ client/src/app/adminhomepage/adminmanagebuses/Adminmanagebuses.js
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useState } from "react";
-import { Card  } from "../../../components/ui/card";
+import { Card, CardContent } from "../../../components/ui/card";
 import { Button } from "../../../components/ui/button";
 import * as Dialog from "@radix-ui/react-dialog";
 
