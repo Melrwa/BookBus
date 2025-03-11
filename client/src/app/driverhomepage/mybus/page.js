a0fe6e71db0138019d377c784c78aa9450339eb7
melrwa <orwamelkizedek@gmail.com>
2025-03-11 06:42:17 +0300

    feat: update driver homepage to display a message instead of the DriverProfile component

--- client/src/app/driverhomepage/mybus/page.js
+++ client/src/app/driverhomepage/mybus/page.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-import DriverProfile from './profile'
+
 
 export default function page() {
   return (
     <div>
-      <DriverProfile />
+     These are my buses
     </div>
   )
 }
